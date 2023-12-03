79a7beb8796a7ee56b4e16bfa44604748cdd7a09
bashirusmans <bashirusman3981@gmail.com>
2023-12-03 15:13:31 +0500

    Testcases

--- ps3/test/expressivo/ExpressionTest.java
+++ ps3/test/expressivo/ExpressionTest.java
@@ -5,6 +5,9 @@ package expressivo;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -23,5 +26,36 @@ public class ExpressionTest {
     
     
     // TODO tests for Expression
+    
+    
+    // Testing for toString
+    @Test
+    public void testToString() {
+        Expression numExp = new NumExp(99);
+        Expression varExp = new VarExp("y");
+
+        assertEquals("99.0", numExp.toString());
+        assertEquals("y", varExp.toString());
+    }
+
+    // Testing for equals
+    @Test
+    public void testEquals() {
+        Expression exp_1 = new NumExp(99);
+        Expression exp_2 = new NumExp(99);
+        Expression exp_3 = new VarExp("y");
+
+        assertEquals(exp_1, exp_2);
+        assertNotEquals(exp_1, exp_3);
+    }
+    @Test
+    public void testHashCode() {
+        Expression exp_1 = new NumExp(99);
+        Expression exp_2 = new NumExp(99);
+        Expression exp_3 = new VarExp("y");
+
+        assertEquals(exp_1.hashCode(), exp_2.hashCode());
+        assertNotEquals(exp_1.hashCode(), exp_3.hashCode());
+    }
     
 }
